941207f9a14c7076bc65c27fc0c6a55411833a22
Gustavo Ferreira <gustavo.oliveira@sou.inteli.edu.br>
2024-02-20 11:28:36 -0300

    fix: json

--- P1/src/publisher.go
+++ P1/src/publisher.go
@@ -3,42 +3,66 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"math/rand"
+	"os"
 	"time"
 )
 
 // SensorData represents the data from the SPS30 sensor
-type SensorData struct {
-	PM1         float64 `json:"pm1"`
-	PM25        float64 `json:"pm25"`
-	PM10        float64 `json:"pm10"`
-	Temperature float64 `json:"temperature"`
-	Humidity    float64 `json:"humidity"`
-	Pressure    float64 `json:"pressure"`
-	Time        string  `json:"time"`
-}
 
 func public(opts *MQTTSubscriber) {
 	singletonClient := opts
 
-	rand.Seed(time.Now().UnixNano())
-	sensorData := SensorData{
-		PM1:         rand.Float64() * 100,
-		PM25:        rand.Float64() * 100,
-		PM10:        rand.Float64() * 100,
-		Temperature: rand.Float64() * 50,  // Temperature in Celsius
-		Humidity:    rand.Float64() * 100, // Relative humidity in percentage
-		Pressure:    rand.Float64() * 2000,
-		Time:        string(time.Now().Format(time.RFC3339)),
-	}
-	jsonData, err := json.Marshal(sensorData)
+	var result []map[string]interface{}
+
+	file, err := os.Open("./data.json")
 	if err != nil {
-		fmt.Println("Error marshalling JSON:", err)
-		return
+		log.Fatalf("Erro ao abrir o arquivo: %s", err)
 	}
 
-	// Publicar usando a instância única do cliente MQTT.
-	token := singletonClient.client.Publish("topic/publisher", 0, false, jsonData)
-	token.Wait()
-	fmt.Println("Publicado:", string(jsonData))
+	defer file.Close()
+
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("Erro ao ler o arquivo: %s", err)
+	}
+
+	err = json.Unmarshal(bytes, &result)
+	if err != nil {
+		log.Fatalf("Erro ao decodificar o JSON: %s", err)
+	}
+	var newObject []map[string]interface{}
+	for _, item := range result {
+		newItem := make(map[string]interface{})
+		for key, value := range item {
+			// Checa se a chave é "Datetime". Se sim, atualiza para o datetime atual.
+			if key == "Datetime" {
+				newItem[key] = time.Now().Format(time.RFC3339)
+			} else {
+				// Para outras chaves, tenta realizar uma operação específica baseada no tipo do valor
+				switch v := value.(type) {
+				case float64:
+					// Se o valor for float64, multiplica por um número aleatório
+					newItem[key] = v * rand.Float64()
+				default:
+					// Para todos os outros tipos, apenas copia o valor
+					newItem[key] = value
+				}
+			}
+		}
+		newObject = append(newObject, newItem)
+	}
+
+	 jsonData, err := json.Marshal(newObject)
+	 if err != nil {
+	 	fmt.Println("Error marshalling JSON:", err)
+	 	return
+	 }
+
+	 // Publicar usando a instância única do cliente MQTT.
+	 token := singletonClient.client.Publish("topic/publisher", 0, false, jsonData)
+	 token.Wait()
+	 fmt.Println("Publicado:", string(jsonData))
 }
