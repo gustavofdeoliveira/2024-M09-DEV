d9ed78e890d53d957f26c79733ea87ac9e9e5c20
Gustavo Ferreira <gustavo.oliveira@sou.inteli.edu.br>
2024-04-07 14:04:07 -0300

    feat: ponderada 7

--- P7/README.md
+++ P7/README.md
@@ -18,11 +18,13 @@ BOOTSTRAP_SERVERS=""
 SASL_USERNAME=""
 SASL_PASSWORD=""
 KAFKA_TOPIC="ponderada"
+MONGO_USER=prodan
+MONGO_PASSWORD=WovV9mriJMNyN2DK
 ```
 
 NOTA: Preencha os campos com as informações do acima.
 
-Após a criação do arquivo `.env`, acesse a página do HiveMQ Cloud e crie uma `Integrations` para o confluent, após ter configurado e criado um cluster no Confluent Cloud e tópico denominado `ponderada`. Na integração, copie o `username`, `password`, `bootstrap` e preencha os campos `Source - Topic` e `Destination Topic` respectivamente com o valor `ponderada`.
+Após a criação do arquivo `.env`, acesse a página do HiveMQ Cloud e crie uma `Integrations` para o confluent, após ter configurado e criado um cluster no Confluent Cloud e tópico denominado `ponderada`. Na integração, copie o `username`, `password`, `bootstrap` e preencha os campos `Source - Topic` e `Destination Topic` respectivamente com o valor `ponderada`. Por fim, crie uma database chamado `db_prodan` e uma collection chamada `ponderada` no MongoDB, não esqueça de criar um usuário e senha para acessar o banco de dados e permitir o acesso do IP da máquina que está rodando o projeto.
 
 
 ## Execução
@@ -45,6 +47,8 @@ Para testar se a integração está funcionando, acesse o HiveMQ Cloud e conecte
 
 Outro teste que pode ser feito é acessar o Confluent Cloud e verificar se o número de mensagem recebidas no tópico `ponderada` aumentou. Ainda no HiveMQ Cloud, é possível verificar se o número de mensagens enviadas aumentou também.
 
+Por fim, acesse o MongoDB e verifique se a collection `ponderada` foi criada e se as mensagens estão sendo salvas corretamente.
+
 ## Demonstração
 
-[Acesse o vídeo de demonstração](https://drive.google.com/file/d/1PqVF06liLjh5QyC7tkKJdTDLgpcf4cim/view?usp=sharing)
+[Acesse o vídeo de demonstração](https://drive.google.com/file/d/1tGMzctc_M7hDEaO4wrg8F0FN3jcqxOUb/view?usp=sharing)
--- P7/src/main.go
+++ P7/src/main.go
@@ -10,7 +10,9 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
-
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 	godotenv "github.com/joho/godotenv"
@@ -121,6 +123,7 @@ func (s *MQTTSubscriber) ReceiveMessage(client MQTT.Client, msg MQTT.Message) {
 		message, err := consumer.ReadMessage(-1)
 		if err == nil {
 			fmt.Printf("[CONSUMER] Received message: %s\n", string(message.Value))
+			inserToMongo(message.Value)
 			
 		} else {
 			fmt.Printf("[CONSUMER] error: %v (%v)\n", err, message)
@@ -129,6 +132,50 @@ func (s *MQTTSubscriber) ReceiveMessage(client MQTT.Client, msg MQTT.Message) {
 	}
 	consumer.Close()
 
+}
+
+func inserToMongo(data []byte) error {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Erro ao carregar o arquivo .env")
+	}
+
+	// Recuperar usuário e senha do arquivo .env
+	mongoUser := os.Getenv("MONGO_USER")
+	mongoPassword := os.Getenv("MONGO_PASSWORD")
+
+	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(fmt.Sprintf("mongodb+srv://%s:%s@prodan.qopo9c4.mongodb.net/?retryWrites=true&w=majority&appName=prodan", mongoUser, mongoPassword)).SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	coll := client.Database("db_prodan").Collection("ponderada")
+	var v interface{}
+	if err := json.Unmarshal(data, &v); err != nil {
+		log.Fatal(err)
+	}
+	json.Unmarshal([]byte(data), &v)
+	result, err := coll.InsertOne(context.TODO(), v)
+
+	if err != nil {
+		panic(err)
+	}
+	
+	if result != nil {
+		fmt.Println("[MONGO] Inserido com sucesso")
+	}
+
+	return nil
 }
 
 func main() {
